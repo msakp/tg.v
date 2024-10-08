3c07598db47f5a5682ff749ba376e53ef070e6d3
msakp <voilond@mail.ru>
2024-10-08 20:06:08 +0300

    pre-mvp

--- main.py
+++ main.py
@@ -6,8 +6,36 @@ from settings import *
 
 
 
-request = requests.get(URL + TEST_CHANNEL, headers=HEADERS)
-print(request.headers)
-soup = Bsoup(request.text, "lxml")
+responce = requests.get(URL + TEST_CHANNEL, headers={"User-Agent": USER_AGENT})
+
+print(f"{responce.status_code} - {RESPONSE_CODE_TABLE[responce.status_code // 100]}")
+
+soup = Bsoup(responce.text, "lxml")
+
+# parse posts and channel info
+
+# main content of tgstat.ru webpage
+containerMain = soup.find("div").find("div").find("div").find_all("div", recursive=False)[1]
+
+channelName = containerMain.find_all("div", recursive=False, limit=2)[1].find("div").find("div").find("div").find_all("div", recursive=False, limit=2)[1].find("h1").text.lstrip().rstrip()
+
+
+# requested channel posts content
+containerPosts = containerMain.find_all("div", recursive=False, limit=3)[2].find("div").find("div").find("div").find("form").find_all("div", recursive=False, limit=2)[1]
+
+# parsing every post
+posts = map(str, filter(lambda div: div.attrs.get("id") and div.attrs["id"].startswith("post-"), containerPosts.find_all("div"))) # converting Tag("div") objects to string
+
+
+#posts = map(str, filter(lambda div: div.attrs.get("id") and div.attrs["id"].startswith("post-"),
+#               soup.find_all("div")))    
+
+
+# writing parsed posts output to log.html
 with open("log.html", 'w', encoding="utf-8") as ofile:
-    ofile.write(soup.prettify())
+    ofile.write('\n'.join(posts))
+
+
+
+print(channelName)
+
